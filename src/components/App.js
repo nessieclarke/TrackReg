7b822c1260cf038834044b9a827f97e906a0df3b
Vanessa Clarke <vanessaclarke@CIC00934.local>
2019-10-23 15:05:23 +0100

    just working on the search button to call profiles

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
-import Data from './Data';
+//import Data from './Data';  this was gonna call in employee data
+import InputGroup from 'react-bootstrap/InputGroup'
 import React, {useState} from 'react';
 
 const employeeProfile = [ 
-  {firstName: "helen", lastName: "clarke", numPlate: "wk57 000", permitNum: 244, workPhone: 07800000000},
-  {firstName: "hannah", lastName: "ruby", numPlate: "B055 000", permitNum: 300, workPhone: 0798000000},
-  {firstName: "vanessa", lastName: "tsang", numPlate: "RO65 000", permitNum: 100, workPhone: 076000000}];
+  {firstName: "helen", lastName: "clarke", numPlate: "wk57 000", permitNum: 244, workPhone: 7800000000},
+  {firstName: "hannah", lastName: "ruby", numPlate: "B055 000", permitNum: 300, workPhone: 798000000},
+  {firstName: "vanessa", lastName: "tsang", numPlate: "RO65 000", permitNum: 100, workPhone: 76000000}];
 
 const CardList = (props) =>(
   <div>
-     <Card {...employeeProfile} />
+     {props.profile.map(profile => <Card {...employeeProfile} />)}
+    
   </div>
 );
-
 
 
 class Card extends React.Component {
@@ -19,7 +20,7 @@ class Card extends React.Component {
     const profile = employeeProfile[0];//this.props; //employeeProfile data
     return(
       <div className="employee-profile">
-        Owner's details...
+        details...
         <div className="info">
           <div className="firstName">{profile.firstName}</div>
           <div className="lastName">{profile.lastName}</div>
@@ -35,24 +36,61 @@ class Card extends React.Component {
 }
 
 class Form extends React.Component {
+  //firstNameInput = React.createRef();   //tried searching the input bar for firstName. 
+  state = {firstName: ''};
+  handleSubmit = (event) => {
+    event.preventDefault();//prevents page refreshing might not need this
+    console.log(
+      //this.firstNameInput.current.value
+    )
+  };
   render () {
     return (
-      <form action="">
-        <input type="text" placeholder='name/reg plate/permit'/>
+      <form onSubmit={}>
+        <input 
+          type="text" 
+          value={this.state.firstName}
+          onChange={event => this.setState({ useName: event.target.value })}
+          placeholder='name/reg plate/permit'
+          //ref={this.firstName}
+          required
+          />
         <button>search</button>
+        
       </form>
     )
   }
 }
+//drop down menu input bar just need to work out what part i need and where to put it.
+/*<InputGroup className="mb-3">
+          <DropdownButton
+              as={InputGroup.Prepend}
+              variant="outline-secondary"
+              title="Dropdown"
+              id="input-group-dropdown-1">
+              <Dropdown.Item href="#">Action</Dropdown.Item>
+              <Dropdown.Item href="#">Another action</Dropdown.Item>
+              <Dropdown.Item href="#">Something else here</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item href="#">Separated link</Dropdown.Item>
+            </DropdownButton>
+          <FormControl aria-describedby="basic-addon1" />
+        </InputGroup>*/
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      profile: employeeProfile,
+    };
+  }
   render(){
     
     return (
       <div>
       <div className="header">{this.props.title}</div>
       <Form />
-      <CardList profile={employeeProfile}/>
+      <CardList profile={this.state.profile}/>
     </div>
     );
   }
